ff1f8a2db82789ab10d8ab2e344d7e51c72114ff
guddn20 <128574570+guddn20@users.noreply.github.com>
2026-06-25 10:37:39 +0900

    update commit

--- Back/back.py
+++ Back/back.py
@@ -2,42 +2,45 @@
 # 1. 필요 모듈 임포트 단계
 # FastAPI의 핵심 클래스인 FastAPI와 요청 객체 처리를 위한 Request를 임포트합니다.
 # URL 경로에서 변수를 추출하고 제약조건을 걸기 위해 Path를 임포트합니다.
-from fastapi import FastAPI, Path
-# from fastapi import Request
-import requests
+from fastapi import FastAPI, Request, Path
 
-url = "http://127.0.0.1:8000/"
+#Path : 함수가 실행될 때 타입을 검사, 정리해주는 역할
+#Field : 백엔드 서버에 들어올 데이터를 명시
 
 # 2. FastAPI 애플리케이션 객체 생성
+# http://127.0.0.1:8000은 이제 'app'이라는 이름으로 부름
 app = FastAPI()
+
+
 # [GET] 기본 루트 경로 ('/') 요청 처리 - 서버가 잘 켜졌는지 확인하는 용도입니다.
 @app.get('/') 
 async def root(): 
 	return {'message' : 'FastAPI 서버가 정상적으로 동작 중입니다!'} 
-
 
 # 복습을 위해 새롭게 설계된 간단한 '강의 데이터베이스(DB)' 대용 딕셔너리입니다.
 course_database = { 101: {'course_id': 101, 'title': '인공지능 입문', 'professor': '박 교수'}, 
 	102: {'course_id': 102, 'title': '파이썬 웹 프로그래밍', 'professor': '김 교수'}, 
 	103: {'course_id': 103, 'title': '자료구조와 알고리즘', 'professor': '이 교수'} } 
 
-response = requests.get(url)
-
 # 4. 경로 변수(Path Variable)를 활용하여 강의 ID를 매개변수로 받는 GET API 라우터를 완성하세요.
 # [요구사항 1] 경로 규칙을 '/course/강의ID' 형태로 변경하세요.
 # [요구사항 2] 강의 ID는 정수형 변수여야 합니다.
 # [요구사항 3] Path() 함수를 사용하여 course_id가 반드시 '100보다 크거나 같은(ge=100)' 정수여야 한다는 제약조건을 추가하세요.
-@app.get('/course/{course_id}') # TOdo: 변경된 경로 규칙을 문자열로 작성하세요 
-async def get_course_info( course_id : int = Path(..., ge=100)): # TOdo: Path를 활용해 최소값 100 제약조건(ge)을 명시하세요. ): 
+@app.get('/course/{course_id}') # TODO: 변경된 경로 규칙을 문자열로 작성하세요  # ... , (필수 파라미터)
+async def get_course_info( course_id : int = Path(..., ge=100)): # TODO: Path를 활용해 최소값 100 제약조건(ge)을 명시하세요. ): 
 
-    # 5. 매개변수로 안전하게 전달받은 course_id를 사용하여 course_database에서 해당 강의 정보를 찾으세요.
-    # 만약에 대비해 딕셔너리의 .get() 메서드를 활용하여 매칭되는 데이터가 없으면 안내 문구를 주도록 응답을 설계합니다.
-    if get_course_info:
-        print(f'조회 성공! 강의 ID {response.json().get('course_id')}는 {response.json().get('professor')}님의 {response.json().get('title')} 입니다.')
-        return get_course_info
+# 5. 매개변수로 안전하게 전달받은 course_id를 사용하여 course_database에서 해당 강의 정보를 찾으세요.
+# 만약에 대비해 딕셔너리의 .get() 메서드를 활용하여 매칭되는 데이터가 없으면 안내 문구를 주도록 응답을 설계합니다.
+    course_info = course_database.get(course_id)
+    
+    if course_info:
+        return course_info
     else:
         return {"error": "해당 강좌 번호는 존재하지 않습니다."}
 
+
+#테스트용 서버 실행
+#uvicorn back:app --reload
 
 # if __name__ == "__main__":
 #     # 강의ID 요청해보기
---
+++ front.py
@@ -0,0 +1,29 @@
+import requests
+
+def request_course_info(url, number):
+    base_url = url + number
+    
+    response = requests.get(base_url)
+    
+    if response.status_code == 200:
+        #정상 응답 받은 결과 -> json 형태로 전달
+        #json의 값은 키와 값의 형태로 오기 때문에 딕셔너리처럼 get 사용
+        result = response.json()
+        title = result.get('title')
+        prof = result.get('professor')
+        print(f'강의 정보 : {title} 과목 , 교수 : {prof}')
+    else:
+        
+        #200~으로 시작하지 않으면 대부분 오류
+        #400 오류 -> 서버
+        #500 오류 -> 프론트 에러
+        
+        print(f'정상 코드를 수신하지 못했습니다. {response.status_code}')
+
+if __name__ == '__main__':
+    target_url = 'http://127.0.0.1:8000/course/'
+    
+    print("강좌를 선택하세요")
+    number = input('강좌 번호 입력 : \n')
+    
+    request_course_info(target_url,number)
